a57efb02ad6dca9d9e4440a57927b22ed7d55bad
Arnon Keereena <arnon.keereena@allianz.com>
2017-04-16 19:06:17 +0200

    resolve wrong callback uri

--- server/controller/gmail/gmail-client.ts
+++ server/controller/gmail/gmail-client.ts
@@ -146,7 +146,7 @@ let G_API_CREDENTIALS = {
         "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
         "client_secret": "-sgMWuS8kIizrT-DlJI8MlAH",
         "redirect_uris": [
-            "http://localhost:3000/oauthcallback"
+            "/oauthcallback"
         ],
         "javascript_origins": [
             "http://localhost:8080",
