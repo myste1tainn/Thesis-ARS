e6f3a6868dc98494a62c8b6e6ab489eb96008414
Arnon Keereena <arnon.keereena@allianz.com>
2017-04-23 13:23:55 +0200

    Add releasing run for faster & more stable run

--- server/server.ts
+++ server/server.ts
@@ -6,6 +6,7 @@ import { ApplicationRestful } from './rest'
 import { SVMRestful } from './svm.rest'
 import { DB } from './collection/db'
 import * as Express from 'express'
+import * as process from 'process'
 
 export let EXPRESS_SERVER = Express()
 console.log('log: bootstrapping express server...')
@@ -24,3 +25,20 @@ console.log(`log: APPLICATION server is now listening on port ${port}`)
 ApplicationRouter.setup(EXPRESS_SERVER)
 ApplicationRestful.setup(EXPRESS_SERVER)
 SVMRestful.setup(EXPRESS_SERVER)
+
+process.stdin.resume();//so the program will not close instantly
+
+function exitHandler(options, err) {
+    if (options.cleanup) console.log('clean');
+    if (err) console.log(err.stack);
+    if (options.exit) process.exit();
+}
+
+//do something when app is closing
+process.on('exit', exitHandler.bind(null,{cleanup:true}));
+
+//catches ctrl+c event
+process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+
+//catches uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
